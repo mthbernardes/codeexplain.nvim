3c819169067fb7bd2343dbbea30ca1d67c36877c
mthbernardes <12648924+mthbernardes@users.noreply.github.com>
2023-05-23 23:10:46 -0300

    test with function

--- rplugin/python3/codeexplain/codeexplain.py
+++ rplugin/python3/codeexplain/codeexplain.py
@@ -7,6 +7,6 @@ class CodeExplain(object):
         self.nvim.command("echom 'My plugin is being executed'")
 
     #@pynvim.autcmd('', pattern='*', eval='', sync=True)
-    @pynvim.command('CodeExplain', range='', nargs='*', sync=True)
+    @pynvim.function('CodeExplain')
     def code_explain(self, filename):
         self.vim.current.buffer[0] = 'Hello, Neovim!'
