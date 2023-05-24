d0524176a00d2dc9a8d2790bfb746698f71b10e7
mthbernardes <12648924+mthbernardes@users.noreply.github.com>
2023-05-24 10:58:04 -0300

    .

--- rplugin/python3/CodeExplain.py
+++ rplugin/python3/CodeExplain.py
@@ -7,8 +7,7 @@ class CodeExplain(object):
         print("Plugin initialized")  # For debugging
         self.nvim.command("echom 'My plugin is being executed'")
 
-    @pynvim.command('CodeExplain', range: True, sync: True)
-    
-    def codeExplain(self,start, stop):
-        print(self.nvim.current.buffer.lines[start-1:stop-1])
-        self.nvim.command("echom 'My plugin is being executed'")
+    @pynvim.command('CodeExplain', range='', sync=True)
+    def codeExplain(self,range):
+        print(self.nvim.current.line = ('Command with range: {}'.format(range)))
+
