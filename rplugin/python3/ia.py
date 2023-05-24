ae4dd7ee411b53eb3c910f6410280c8860736941
mthbernardes <12648924+mthbernardes@users.noreply.github.com>
2023-05-24 11:53:57 -0300

    .

--- rplugin/python3/ia.py
+++ rplugin/python3/ia.py
@@ -1,29 +1,3 @@
-import langchain
-from langchain.cache import InMemoryCache
-from langchain.llms import LlamaCpp
-from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-class IA():
-    def __init__(self,):
-        langchain.llm_cache = InMemoryCache()
-        LLAMA_EMBEDDINGS_MODEL = '/Users/matheus.bernardes/dev/mthbernardes/code-query/models/ggml-model-q4_0.bin' 
-        MODEL_N_CTX = 1000
-        CALLBACKS = [StreamingStdOutCallbackHandler()]
-        PROMPT_TEMPLATE = """Act as a expert software engineer with high skill in software quality, performance and security. I will provide a piece of code to you and it is your job to explain it. If you can not explain the code just answer with "Sorry I was unable to understand it." 
-        ```
-        {code}
-        ```
-        """
-        PROMPT = PromptTemplate(template=PROMPT_TEMPLATE, input_variables=["code"])
-        LLM = LlamaCpp(model_path=LLAMA_EMBEDDINGS_MODEL, n_ctx=MODEL_N_CTX, verbose=False)
-        self.CHAIN = LLMChain(llm=LLM, prompt=PROMPT,memory=ConversationBufferMemory())
-
-    def run(self,code):
-        return self.CHAIN.run(code)
 
 
 #code =  """ 
