1a3929b1b4b625a5197ec2fe277f11418071e850
mthbernardes <12648924+mthbernardes@users.noreply.github.com>
2023-05-23 23:00:55 -0300

    add log

--- rplugin/python3/codeexplain/codeexplain.py
+++ rplugin/python3/codeexplain/codeexplain.py
@@ -3,6 +3,7 @@ import pynvim
 @pynvim.plugin
 class CodeExplain(object):
     def __init__(self, nvim):
+        self.nvim.command("echom 'My plugin is being executed'")
         self.nvim = nvim
 
     #@pynvim.autcmd('', pattern='*', eval='', sync=True)
