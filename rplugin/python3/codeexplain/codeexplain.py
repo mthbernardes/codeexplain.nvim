d2eae5cf7bc4a2079777ec88db6da48a91e4bf46
mthbernardes <12648924+mthbernardes@users.noreply.github.com>
2023-05-23 22:58:06 -0300

    rplugin

---
+++ rplugin/python3/codeexplain/codeexplain.py
@@ -0,0 +1,11 @@
+import pynvim
+
+@pynvim.plugin
+class CodeExplain(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    #@pynvim.autcmd('', pattern='*', eval='', sync=True)
+    @pynvim.command('CodeExplain', range='', nargs='*', sync=True)
+    def code_explain(self, filename):
+        self.vim.current.buffer[0] = 'Hello, Neovim!'
